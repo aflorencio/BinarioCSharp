8ba240950a77b805fcebd4f777b3cccdea2b2eef
we <e>
2019-11-21 12:10:14 +0100

    .Git Bin example

---
+++ Binario/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Binario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "datos.txt";
+
+            FileStream stream = new FileStream(fileName,
+                FileMode.Open, FileAccess.Read);
+            BinaryReader reader = new BinaryReader(stream);
+
+            Console.Write(reader.ReadString());
+            Console.Write(reader.ReadString());
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write("[{0}]", reader.ReadDouble());
+                }
+                Console.Write(reader.ReadString());
+            }
+            reader.Close();
+            stream.Close();
+            Console.ReadKey();
+        }
+    }
+}
